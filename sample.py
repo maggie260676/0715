68ef0aa90adb317a962d83201b62a6552b33cf35
maggie260676 <52891094+maggie260676@users.noreply.github.com>
2019-07-15 11:44:22 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -9,6 +9,8 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, ImageMessage, ImageSendMessage,
+    MessageEvent, StickerMessage, StickerSendMessage,
 )
 
 app = Flask(__name__)
@@ -42,14 +44,14 @@ def handle_message(event):
     # 回應使用者輸入的話
     if "貼圖" in msg or "sticker" in msg:
         message = StickerSendMessage(
-        package_id = '1',
-        sticker_id = '1'
+            package_id = '1',
+            sticker_id = '1'
          )
         line_bot_api.reply_message(event.reply_token,message)
     elif "圖片" in msg or 'https://example.com/original.jpg' in msg:
         message = ImageSendMessage(
-        original_content_url = 'https://example.com/original.jpg',
-        preview_image_url = 'https://example.com/preview.jpg'
+            original_content_url = 'https://example.com/original.jpg',
+            preview_image_url = 'https://example.com/preview.jpg'
         )
         line_bot_api.reply_message(event.reply_token,message)
     else:
